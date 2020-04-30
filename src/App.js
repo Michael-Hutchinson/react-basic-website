e51551d9eda4fae495c47ce78ae371f587419b1e
Michael Hutchinson <michaelhutchinson@michaels-mbp.mynet>
2020-04-30 18:08:11 +0100

    Install Gulp, boostrap & scss
    - Added Bootstrap as frontend CSS framework
    - Added SCSS as my CSS pre-processor
    - Added Gulp for compiler and watcher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'; 
+import React from 'react';
+
+import './Assets/css/default.min.css';
 
 function App() {
   return (
     <div className="App">
+      <p>Test</p>
     </div>
   );
 }
